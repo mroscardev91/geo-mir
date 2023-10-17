6f60c8d16e8f6b4fad5d7d04466e7209a1344a1a
alexmpDev <amparedes2004@gmail.com>
2023-10-17 18:58:17 +0200

    Codi base del projecte

---
+++ projecte/config/admin.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+   'name'      => env('ADMIN_NAME', 'admin'),
+   'email'     => env('ADMIN_EMAIL', 'almapa@fp.insjoaquimmir.cat'),
+   'password'  => env('ADMIN_PASSWORD', 'admin1234'),
+];
