6a75e8e56f4f1dce206544320b30d972b021d610
oscargdzs <ogomezd@fp.insjoaquimmir.cat>
2023-10-18 19:38:54 +0200

    verificacio correu i migracions de rols usuaris

---
+++ database/migrations/2023_10_18_173321_create_roles_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('roles', function (Blueprint $table) {
+            $table->id(); // columna id, clave primaria y autoincremental
+            $table->string('name')->unique(); // columna name con restricción de unicidad
+            $table->timestamps();
+        });
+
+        Schema::table('users', function (Blueprint $table) {
+            $table->unsignedBigInteger('role_id')->nullable(); // columna role_id con opción de valores null
+            $table->foreign('role_id')->references('id')->on('roles')->onDelete('cascade');
+            // clave foránea para relacionar role_id con id de la tabla roles
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+{
+    Schema::table('users', function (Blueprint $table) {
+        // Eliminar la clave foránea
+        $table->dropForeign(['role_id']);
+        // Eliminar la columna role_id
+        $table->dropColumn('role_id');
+    });
+
+    // Eliminar la tabla roles
+    Schema::dropIfExists('roles');
+}
+};
