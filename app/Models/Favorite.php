b65f6efd7fd10dd66465a7b9a1cddcd6303e601f
oscargdzs <ogomezd@fp.insjoaquimmir.cat>
2023-10-30 18:58:52 +0100

    model creat: place, favorite, review

--- app/Models/Favorite.php
+++ app/Models/Favorite.php
@@ -8,4 +8,18 @@ use Illuminate\Database\Eloquent\Model;
 class Favorite extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['user_id', 'place_id'];
+
+    // Relació amb la taula users
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    // Relació amb la taula places
+    public function place()
+    {
+        return $this->belongsTo(Place::class);
+    }
 }
--- app/Models/Place.php
+++ app/Models/Place.php
@@ -8,4 +8,24 @@ use Illuminate\Database\Eloquent\Model;
 class Place extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['name', 'description', 'file_id'];
+
+    // Relació amb la taula files
+    public function file()
+    {
+        return $this->belongsTo(File::class);
+    }
+
+    // Relació amb la taula reviews
+    public function reviews()
+    {
+        return $this->hasMany(Review::class);
+    }
+
+    // Relació amb la taula favorites
+    public function favorites()
+    {
+        return $this->hasMany(Favorite::class);
+    }
 }
